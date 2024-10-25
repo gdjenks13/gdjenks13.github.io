e64dfd4dfa7ec95fe59847a44b2c9a5e30b63b44
gdjenks13 <gdjenks13@gmail.com>
2024-10-25 09:29:47 -0400

    improved responsiveness on small screens, more cleanup

--- js/header.js
+++ js/header.js
@@ -2,11 +2,23 @@ let lastScrollY = window.scrollY;
 
 window.addEventListener('scroll', () => {
     const header = document.querySelector('header');
-    
-    if (window.scrollY > lastScrollY) {
-        header.classList.add('header-hidden');
+
+    if (window.innerWidth >= 768) {
+        if (window.scrollY > lastScrollY) {
+            header.classList.add('header-hidden');
+        } else {
+            header.classList.remove('header-hidden');
+        }
     } else {
         header.classList.remove('header-hidden');
     }
+    
     lastScrollY = window.scrollY;
+});
+
+window.addEventListener('resize', () => {
+    const header = document.querySelector('header');
+    if (window.innerWidth < 768) {
+        header.classList.remove('header-hidden');
+    }
 });
---
+++ js/templating.js
@@ -0,0 +1,21 @@
+function loadHTMLContent(fileName, containerId) {
+    fetch(fileName)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error("Network response was not ok");
+            }
+            return response.text();
+        })
+        .then(data => {
+            document.getElementById(containerId).innerHTML = data;
+        })
+        .catch(error => {
+            console.error("Failed to load content:", error);
+        });
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadHTMLContent('templates/about_me.html', 'about_me');
+    loadHTMLContent('templates/experience.html', 'experience');
+    loadHTMLContent('templates/education.html', 'education');
+});
